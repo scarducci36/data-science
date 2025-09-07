34bd17accc81e166cdc97d36595671063ee606ed
Stephanie Carducci <stephanielynncarducci@gmail.com>
2025-09-07 16:05:53 -0700

    Correctign some syntax errors

--- data-science/data-processing.py
+++ data-science/data-processing.py
@@ -3,21 +3,22 @@
 
 import requests 
 import base64
+import os 
+from dotenv import load_dotenv
 
-CLIENT_ID = "3a2158dfdfee40bda1b696967040373f"
-CLIENT_SECRET = ""
+load_dotenv()
+
+CLIENT_ID = os.getenv("CLIENT_ID")
+CLIENT_SECRET = os.getenv("CLIENT_SECRET")
 
 auth_str = f"{CLIENT_ID}:{CLIENT_SECRET}"
 b64_auth_str = base64.b64encode(auth_str.encode()).decode()
-
-
-
-ACCESS_TOKEN = ""
 
 #Example search: URL searching for artist 
 url_token = "https://accounts.spotify.com/api/token"
 headers_b64 = {"Authorization": f"Basic {b64_auth_str}", 
                "Content-Type": "application/x-www-form-urlencoded"}
+
 data = {"grant_type": "client_credentials"}
 
 response = requests.post(url_token,headers=headers_b64, data=data)
