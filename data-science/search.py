cbe44931c72b7986e280eefedc3fb07294b311a6
Stephanie Carducci <stephanielynncarducci@gmail.com>
2025-09-10 11:55:26 -0700

    changes

--- data-science/search.py
+++ data-science/search.py
@@ -18,12 +18,12 @@ def refresh_token(url, headers, data):
 def search_request(url, headers, params):
     return requests.get(url, headers=headers, params=params); 
 
-def save_search(data):
-    artist_name = data["artists"]['name']
-    genres = data["artists"]["genres"]
-    followers = data["artists"]["followers"]
-    popularity = data["artists"]["popularity"]
-    id = data["artists"]["id"]
+def save_search(data, result_num):
+    artist_name = data["artists"][result_num]['name']
+    genres = data["artists"][result_num]["genres"]
+    followers = data["artists"][result_num]["followers"]
+    popularity = data["artists"][result_num]["popularity"]
+    id = data["artists"][result_num]["id"]
     search_time = time.time
 
     connection = sqlite3.connect("spotify_searches.db")
@@ -96,7 +96,7 @@ if response.status_code == 200:
     artists = data['artists']['items']
     num = 5
     for i in range(num):
-        save_search(data)
+        save_search(data, i)
         print("Artist ", i+1)
         print("Name: ", artists[i]["name"])
         print("ID: ", artists[i]["id"])
