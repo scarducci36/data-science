ea4b49df0a2c8f263f26a1aa48097cc87cddf6c1
Stephanie Carducci <stephanielynncarducci@gmail.com>
2025-09-13 17:34:32 -0700

    turning genre list into string

--- data-science/home.py
+++ data-science/home.py
@@ -41,7 +41,7 @@ def save_search(data):
 
             else: 
                 c.execute("INSERT INTO artists (artist_name, genres, followers, popularity, spotify_id, last_updated) VALUES (?, ?, ?, ?, ?, ?)", 
-                        (artist['name'], artist['genres'], artist['followers']['total'], artist['popularity'], artist['id'], search_time))
+                        (artist['name'], ", ".join(artist['genres']), artist['followers']['total'], artist['popularity'], artist['id'], search_time))
                 print("Artist ", i)
                 i = i + 1
                 print("Name: ", artist["name"])
